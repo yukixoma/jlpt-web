b277f0b16142a62c9aa57c3429fda34adc91f485
Ha Viet Huy Phat <yukixoma08@gmail.com>
2019-07-24 21:10:32 +0700

    Updates

---
+++ precache-manifest.4747d453ae551e649d9019b18d97d5d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b778cc7b94a2fb3714ad9055e6d3b30d",
+    "url": "/jlpt-web/index.html"
+  },
+  {
+    "revision": "ca3f45cddc8ea9699b6f",
+    "url": "/jlpt-web/static/css/main.73ffd13f.chunk.css"
+  },
+  {
+    "revision": "f21796a5e5997c36e4dc",
+    "url": "/jlpt-web/static/js/2.c4494dbe.chunk.js"
+  },
+  {
+    "revision": "ca3f45cddc8ea9699b6f",
+    "url": "/jlpt-web/static/js/main.d629266e.chunk.js"
+  },
+  {
+    "revision": "4a186863f77e44d244a2",
+    "url": "/jlpt-web/static/js/runtime~main.6af0e0f2.js"
+  },
+  {
+    "revision": "0f7f60274d714a3bf8db3860863d6f69",
+    "url": "/jlpt-web/static/media/guoyu.0f7f6027.woff2"
+  }
+]);
